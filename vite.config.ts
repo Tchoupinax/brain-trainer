cd50487272bc5d4a9e55a88579c49cc9e6dd1b0b
Tchoupinax <corentinfiloche@hotmail.fr>
2026-05-02 08:23:43 +0200

    chore: debug CI

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 const base = process.env.GITHUB_PAGES_BASE || "/";
-
+console.log("base", base);
 export default defineConfig({
   plugins: [react()],
   base,
